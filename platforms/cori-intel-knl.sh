8a8f19f0d41217d5bfeac4d05c9d8f9b191d50aa
Theodore Kisner <tskisner.public@gmail.com>
2018-02-26 06:26:57 -0800

    Tweak Intel compiler builds.

--- platforms/cori-intel-knl.sh
+++ platforms/cori-intel-knl.sh
@@ -5,7 +5,7 @@ OPTS="$@"
 cmake \
 -DCMAKE_C_COMPILER="cc" \
 -DCMAKE_CXX_COMPILER="CC" \
--DCMAKE_C_FLAGS="-O3 -g -fPIC -xmic-avx512 -pthread" \
--DCMAKE_CXX_FLAGS="-O3 -g -fPIC -xmic-avx512 -pthread" \
+-DCMAKE_C_FLAGS="-O3 -g -no-gcc -fPIC -xmic-avx512 -pthread" \
+-DCMAKE_CXX_FLAGS="-O3 -g -no-gcc -fPIC -xmic-avx512 -pthread" \
 ${OPTS} \
 ..
--- platforms/edison-intel.sh
+++ platforms/edison-intel.sh
@@ -5,7 +5,7 @@ OPTS="$@"
 cmake \
 -DCMAKE_C_COMPILER="cc" \
 -DCMAKE_CXX_COMPILER="CC" \
--DCMAKE_C_FLAGS="-O3 -g -fPIC -xavx -pthread" \
--DCMAKE_CXX_FLAGS="-O3 -g -fPIC -xavx -pthread" \
+-DCMAKE_C_FLAGS="-O3 -g -no-gcc -fPIC -xavx -pthread" \
+-DCMAKE_CXX_FLAGS="-O3 -g -no-gcc -fPIC -xavx -pthread" \
 ${OPTS} \
 ..

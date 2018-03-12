ba2b945cb2974e79d8ee6db5a540eb6961e5e327
Theodore Kisner <tskisner.public@gmail.com>
2018-03-12 13:25:11 -0700

    Update configurations for Intel compiler.

--- platforms/edison-intel.sh
+++ platforms/edison-intel.sh
@@ -2,12 +2,14 @@
 
 OPTS="$@"
 
+LDFLAGS='-dynamic' \
 cmake \
 -DCMAKE_C_COMPILER="cc" \
 -DCMAKE_CXX_COMPILER="CC" \
 -DMPI_C_COMPILER="cc" \
 -DMPI_CXX_COMPILER="CC" \
--DCMAKE_C_FLAGS="-O3 -g -fPIC -xavx -pthread" \
--DCMAKE_CXX_FLAGS="-O3 -g -fPIC -xavx -pthread" \
+-DCMAKE_C_FLAGS="-O3 -g -fPIC -xavx -dynamic -pthread -DSQLITE_DISABLE_INTRINSIC" \
+-DCMAKE_CXX_FLAGS="-O3 -g -fPIC -xavx -dynamic -pthread -DSQLITE_DISABLE_INTRINSIC" \
+-DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
 ${OPTS} \
 ..
